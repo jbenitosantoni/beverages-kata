0048e4078050fc207aa3425bb7409fea9424a26b
Javier Benito Santoni <javier@javierbenito.me>
2021-04-28 18:21:02 +0200

    Add types

--- ClientCodeGenerator.php
+++ ClientCodeGenerator.php
@@ -3,14 +3,16 @@
 // Examples of generated beverages
 final class ClientCodeGenerator
 {
-    public static function createCoffeWithMilkAndCream() {
+    public static function createCoffeWithMilkAndCream(): AbstractBeverage
+    {
         $builder = self::generateBuilder(new Coffee());
         $builder->addCream();
         $builder->addMilk();
         return $builder->getBeverage();
     }
 
-    private static function generateBuilder(AbstractBeverage $beverage) {
+    private static function generateBuilder(AbstractBeverage $beverage): BeverageBuilder
+    {
         return new BeverageBuilder($beverage);
     }
 }
